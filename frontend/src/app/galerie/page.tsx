dac57663d628d6ae4939797d62101b0b649cedf9
LucasMaroy <lucasmaroy42@gmail.com>
2025-09-22 23:19:10 +0200

    feat(frontend): init a galerie page

---
+++ frontend/src/app/galerie/page.tsx
@@ -0,0 +1,25 @@
+import { getNavMenu, getInfo } from "@/lib/data";
+import Footer from "@/ui/components/footer";
+import Header from "@/ui/components/client-header";
+import Form from "@/ui/components/form";
+
+export default async function Page() {
+    const image01 = "/logo_tavola_colo_02_dark.svg";
+    const image01ALT = "Logo Tavola";
+    const hoursInfo = await getInfo();
+    const menuNav = await getNavMenu();
+    return (
+        <main className="h-100vh flex flex-1 flex-col">
+            <Header listMenu={menuNav} />
+            <section className="flex flex-1 flex-col">
+                <p>Galerie</p>
+            </section>
+            <Footer
+                listMenu={menuNav}
+                imageSrc={image01}
+                imageAlt={image01ALT}
+                openHours={hoursInfo}
+            />
+        </main>
+    );
+}
