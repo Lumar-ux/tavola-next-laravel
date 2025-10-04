447edff0803e4cbdfbff45773def06664658fd58
LucasMaroy <lucasmaroy42@gmail.com>
2025-10-04 17:33:50 +0200

    fix(main-card): add navigation link to CTA button

--- frontend/src/components/ui/main-card.tsx
+++ frontend/src/components/ui/main-card.tsx
@@ -1,10 +1,11 @@
 import { CardProps } from "@/lib/definitions";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function MainCard({ title, body, dateUpdate }: CardProps) {
     return (
-        <div className="bg-tavo-light flex size-full flex-col justify-between rounded-xl p-28 lg:w-10/12 xl:h-[26.5rem] 2xl:h-[23.875rem]">
-            <div>
+        <div className="bg-tavo-light flex size-full flex-col items-start justify-between rounded-xl p-28 lg:w-10/12 xl:h-[26.5rem] 2xl:h-[23.875rem]">
+            <div className="focus-within:border-ring focus-within:ring-ring flex flex-col items-start focus-within:ring-[3px] focus-within:outline-none">
                 <h2
                     className={
                         "text-tavo-dark mb-18 text-4xl font-bold uppercase md:mb-32 md:text-5xl"
@@ -15,12 +16,16 @@ export default function MainCard({ title, body, dateUpdate }: CardProps) {
                 <p className="text-normal columns-1 gap-8 text-sm md:columns-2 md:text-base">
                     {body}
                 </p>
-                <hr className="my-10 h-2 bg-tavo-gray md:my-18" />
-                <p className="pb-4 text-xs text-tavo-gray md:pb-0 md:text-sm">
+                <hr className="bg-tavo-gray my-10 h-2 w-full md:my-18" />
+                <p className="text-tavo-gray pb-4 text-xs md:pb-0 md:text-sm">
                     {dateUpdate}
                 </p>
             </div>
-            <Button>Learn More</Button>
+            <Button asChild>
+                <Link className="stretched-link" href="/restaurant">
+                    Learn More
+                </Link>
+            </Button>
         </div>
     );
 }
