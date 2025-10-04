94bf6ad9176b49b0a9e50754f92163638c010b1a
LucasMaroy <lucasmaroy42@gmail.com>
2025-10-04 15:08:35 +0200

    feat(restaurant): expose map iframe src via API endpoint to frontend

--- backend/config/info.php
+++ backend/config/info.php
@@ -5,32 +5,34 @@ return [
         'localisation' => 'Vancouver, Ca',
         'address' => '1021 Burnaby St',
         'openHours' => [
-            'label'=> "Ouverture",
+            'label'=> "Heure Ouverture",
             'weekDays' => [
                 'days'  => 'Monday-Friday',
                 'hours' => '2:00 PM - 12:00 PM',
             ],
             'weekend' => [
-                'days'  => 'weekend',
+                'days'  => 'Weekend',
                 'hours' => '12:00 AM - 12:00 PM',
             ],
         ],
+        'srcIframe'=> "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2602.864707650293!2d-123.13347752302398!3d49.27896077101716!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x548673d35ecb324b%3A0x4b460c69c0f43760!2s1021%20Burnaby%20St%2C%20Vancouver%2C%20BC%20V6E%201N9%2C%20Canada!5e0!3m2!1sfr!2sbe!4v1717749318188!5m2!1sfr!2sbe",
         'phone' => '+1 604-123-4567',
     ],
     [
         'localisation' => 'Portland, USA',
         'address' => '10026 SE Division St',
         'openHours' => [
-            'label'=> "Ouverture",
+            'label'=> "Heure Ouverture",
             'weekDays' => [
                 'days'  => 'Monday-Friday',
                 'hours' => '2:00 PM - 11:00 PM',
             ],
             'weekend' => [
-                'days'  => 'weekend',
+                'days'  => 'Weekend',
                 'hours' => '12:00 AM - 11:00 PM',
             ],
         ],
+        'srcIframe'=> "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3954.5750934738967!2d-122.56021942302927!3d45.50390097560535!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5495a1b6515bc815%3A0x3901c67943a9cdfa!2s10026%20SE%20Division%20St%2C%20Portland%2C%20OR%2097266!5e0!3m2!1sen!2sus!4v1759583026196!5m2!1sen!2sus",
         'phone' => '+1 573-123-4567',
     ],
 ];
