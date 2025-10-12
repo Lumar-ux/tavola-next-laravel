cea2a0717be9ac798377db5cbd65ed2e99d37b62
LucasMaroy <lucasmaroy42@gmail.com>
2025-10-12 18:44:55 +0200

    fix(Header): rename 'client-header' to 'header-client'

--- frontend/src/components/ui/header.tsx
+++ frontend/src/components/ui/header.tsx
@@ -1,5 +1,5 @@
 import { menuNavType } from "@/lib/definitions";
-import HeaderClient from "@/components/ui/client-header";
+import HeaderClient from "@/components/ui/header-client";
 
 export default async function Header({ listMenu }: { listMenu: menuNavType }) {
     return <HeaderClient data={listMenu} />;
